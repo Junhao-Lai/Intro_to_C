e7de8474956f5c2c630b276f2bac967130e85980
Junhao Lai <114185319+Junhao-Lai@users.noreply.github.com>
2023-01-08 08:09:53 +0000

    double and cube added

--- .ccls-cache/@home@runner@wk4CProgramming/wk4_stuff@DoubleCube.c
+++ .ccls-cache/@home@runner@wk4CProgramming/wk4_stuff@DoubleCube.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 
+double square( double);
+double cube( double);
+
+
+int main()
+{
+  int how_many = 0, i, j;
+  printf("Square and Cube from 1 to n, where n is ?");
+  scanf("%d",&how_many);
+  printf("\n square and cubes by interval of 0.1\n");
+
+  for(i=1;i<=how_many;i++)
+    for(j=0; j<10; j++)
+      printf("\n%lf\t %lf\t%lf",
+        i + j/10.0, square(i + j/10.0), cube(i + j/10.0));
+
+  printf("\n\n");
+  return 0;
+}
+
+
+double square( double x)
+{
+  return(x*x);
+}
+
 double cube(double x)
 {
   return(x*x*x);
 }
-
-a = cube(3.5);
 
